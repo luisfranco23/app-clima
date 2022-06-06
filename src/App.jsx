a8ad2a2c68450de168ca1d7e2b8925a8ec71956d
Luis Franco <lafrancol@unitecnica.net>
2022-06-05 22:42:24 -0500

    agregando carga inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
 import Climate from "./components/Climate";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    },3000)
+  }, [])
+
   return (
     <div className="App">
-      <Climate />
+      {
+       loading ? (
+         <ClimbingBoxLoader color={'#000'} loading={loading} size={20} />
+         ) 
+         :(
+           <Climate />
+        )
+      }
     </div>
   );
 }
